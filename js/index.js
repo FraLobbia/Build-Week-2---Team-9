4a2561ef208f095b58bd0cb2902436c5f225a31f
Stefano <dantoni.stefano@libero.it>
2023-12-14 14:41:08 +0100

    post pranzo

--- js/index.js
+++ js/index.js
@@ -5,20 +5,18 @@ const audio = document.getElementById("myAudio");
 const allPlayButtons = document.getElementsByClassName("playButtonClass");
 
 document.addEventListener("DOMContentLoaded", function () {
-	const volumeSlider = document.querySelector(".volume-slider");
-	const progressBar = document.querySelector("#progressBar");
+  const volumeSlider = document.querySelector(".volume-slider");
+  const progressBar = document.querySelector("#progressBar");
 
-	volumeSlider.addEventListener("input", function () {
-		const volumePercentage =
-			((this.value - this.min) / (this.max - this.min)) * 100;
-		this.style.setProperty("--volume-percentage", `${volumePercentage}%`);
-	});
+  volumeSlider.addEventListener("input", function () {
+    const volumePercentage = ((this.value - this.min) / (this.max - this.min)) * 100;
+    this.style.setProperty("--volume-percentage", `${volumePercentage}%`);
+  });
 
-	progressBar.addEventListener("input", function () {
-		const volumePercentage =
-			((this.value - this.min) / (this.max - this.min)) * 100;
-		this.style.setProperty("--volume-percentage", `${volumePercentage}%`);
-	});
+  progressBar.addEventListener("input", function () {
+    const volumePercentage = ((this.value - this.min) / (this.max - this.min)) * 100;
+    this.style.setProperty("--volume-percentage", `${volumePercentage}%`);
+  });
 });
 
 const discoverOtherArtist = document.getElementById("discoverOtherArtist");
@@ -56,92 +54,6 @@ const options = {
 const arrayAlbums = [74311, 45122, 52133, 54404, 51505, 68884];
 createAlbum(5);
 
-<<<<<<< Updated upstream
-function createAlbum(quantity) {
-	for (let i = 0; i < quantity; i++) {
-		const albumCasuale =
-			Math.floor(Math.random() * (75000 - 10000 + 1)) + 10000;
-		const albumID = arrayAlbums[i];
-		const endpointAlbum = `https://deezerdevs-deezer.p.rapidapi.com/album/${albumID}`;
-
-		fetch(endpointAlbum, {
-			method: "GET",
-			headers: {
-				"X-RapidAPI-Key": token,
-				"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-			},
-		})
-			.then((response) => {
-				if (!response.ok) {
-					console.log(response);
-					if (response.status === 429) {
-						throw response.status;
-					}
-					createAlbum(1);
-					throw response.status;
-				}
-				return response.json();
-			})
-			.then((album) => {
-				console.log(album);
-				fillHeroSection(album);
-				const elemento = document.createElement("div");
-				elemento.className = "col-6 card bg-dark p-2 m-auto";
-				elemento.innerHTML = `
-                        <a href="./artist.html?albumCasuale=${albumCasuale}">
-                            <img
-                                src="${album.artist.picture_xl}"              
-                                class="card-img-top"
-                                alt="copertina album/playlist"
-                            />
-                            <h6 class="card-title text-white fw-bold mt-3 mb-2">
-                                ${album.artist.name}
-                            </h6>
-                            <p class="card-text text-secondary">
-                                Album totali ${album.nb_tracks}
-                            </p> 
-                        </a>    
-        `;
-				discoverOtherArtist.appendChild(elemento);
-			})
-			.catch((errorCode) => {
-				switch (errorCode) {
-					case 404:
-						message =
-							"🤬🤬🤬🤬 Risorsa non trovata. Codice errore: ";
-						console.log(message + errorCode);
-						break;
-					case 401:
-						message =
-							"🤬🤬🤬🤬 Non sei autorizzato. Codice errore: ";
-						console.log(message + errorCode);
-						break;
-					case 418:
-						message = "🤬🤬🤬🤬 I'm a teapot. Codice errore: ";
-						console.log(message + errorCode);
-						break;
-					case 429:
-						message = "🤬🤬🤬🤬 Troppe richieste: ";
-						console.log(message + errorCode);
-						break;
-					case 500:
-						message = "🤬🤬🤬🤬 Errore del server: ";
-						console.log(message + errorCode);
-						break;
-					case 800:
-						message = "🤬🤬🤬🤬 Non ci sono dati!!!: ";
-						console.log(message + errorCode);
-						break;
-					default:
-						message =
-							"❌❌❌❌ Errore con codice non definito. Codice errore: ";
-						console.log(message + errorCode);
-						createAlbum(1);
-						break;
-				}
-			});
-	}
-=======
 function createArtist(quantity) {
   for (let i = 0; i < quantity; i++) {
     const albumCasuale = Math.floor(Math.random() * (75000 - 10000 + 1)) + 10000;
@@ -218,42 +130,39 @@ function createArtist(quantity) {
         }
       });
   }
->>>>>>> Stashed changes
 }
 
 ///////////////////////////////////////////////////////////////////////
 
 function fillHeroSection(album) {
-	const spansArtist = document.getElementsByClassName("artistHeroSection");
-	const imgHeroSection = document.getElementById("imgHeroSection");
-	const spanAlbumHeroSection = document.getElementById(
-		"spanTrackHeroSection"
-	);
+  const spansArtist = document.getElementsByClassName("artistHeroSection");
+  const imgHeroSection = document.getElementById("imgHeroSection");
+  const spanAlbumHeroSection = document.getElementById("spanTrackHeroSection");
 
-	for (const span of spansArtist) {
-		span.innerHTML = album.artist.name;
-	}
+  for (const span of spansArtist) {
+    span.innerHTML = album.artist.name;
+  }
 
-	const n = Math.floor(Math.random() * 5);
-	const trackSceltaCasualmente = album.tracks.data[n];
-	spanTrackHeroSection.innerHTML = trackSceltaCasualmente.title;
-	imgHeroSection.setAttribute("src", album.cover_big);
+  const n = Math.floor(Math.random() * 5);
+  const trackSceltaCasualmente = album.tracks.data[n];
+  spanTrackHeroSection.innerHTML = trackSceltaCasualmente.title;
+  imgHeroSection.setAttribute("src", album.cover_big);
 
-	riempiDataPlayer(trackSceltaCasualmente);
+  riempiDataPlayer(trackSceltaCasualmente);
 }
 
 function riempiDataPlayer(track) {
-	const dataPlayer = document.getElementById("dataPlayer");
-	const previewAudioLink = track.preview;
+  const dataPlayer = document.getElementById("dataPlayer");
+  const previewAudioLink = track.preview;
 
-	// vado a predisporre l'audio da riprodurre
-	audio.innerHTML = `
+  // vado a predisporre l'audio da riprodurre
+  audio.innerHTML = `
     <source id="audioSource" src="${previewAudioLink}" type="audio/mp3" />
     `;
 
-	// vado a riempire il player con i dati del'artista, titolo track e copertina album
-	dataPlayer.innerHTML = ""; // prima lo svuoto dal riempimento fatto alla prima apertura della pagina
-	dataPlayer.innerHTML = `
+  // vado a riempire il player con i dati del'artista, titolo track e copertina album
+  dataPlayer.innerHTML = ""; // prima lo svuoto dal riempimento fatto alla prima apertura della pagina
+  dataPlayer.innerHTML = `
         
                 <img
                     src="${track.album.cover_small}"
@@ -277,23 +186,23 @@ function riempiDataPlayer(track) {
                     <i class="bi bi-heart"></i>
                 </button>
     `;
-	for (const button of allPlayButtons) {
-		button.addEventListener("click", play);
-	}
+  for (const button of allPlayButtons) {
+    button.addEventListener("click", play);
+  }
 }
 
 // -----------------------------------------------
 
 function play() {
-	audio.play();
-	for (const button of allPlayButtons) {
-		button.addEventListener("click", pause);
-	}
+  audio.play();
+  for (const button of allPlayButtons) {
+    button.addEventListener("click", pause);
+  }
 }
 function pause() {
-	audio.pause();
-	console.log("pausa");
-	for (const button of allPlayButtons) {
-		button.addEventListener("click", play);
-	}
+  audio.pause();
+  console.log("pausa");
+  for (const button of allPlayButtons) {
+    button.addEventListener("click", play);
+  }
 }
